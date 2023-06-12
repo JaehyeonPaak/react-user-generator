a84a2ae558accf28c7011709046d46aee09d5466
wogus392 <wogus392@nexol.co.kr>
2023-06-12 17:43:17 +0900

    style: moving modal and backdrop to another tree by using portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,22 +2,27 @@ import Card from './Card.js';
 import Button from './Button.js';
 import styles from './ErrorModal.module.css';
 import React from 'react';
+import ReactDom from 'react-dom';
 
 const ErrorModal = (props) => {
     return (
         <React.Fragment>
-            <div className={styles.backdrop} onClick={props.onClick}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onClick}>OK</Button>
-                </footer>
-            </Card>
+            {ReactDom.createPortal((
+                <div className={styles.backdrop} onClick={props.onClick}></div>), 
+            document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal((
+                <Card className={styles.modal}>
+                    <header className={styles.header}>
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div className={styles.content}>
+                        <p>{props.message}</p>
+                    </div>
+                    <footer className={styles.actions}>
+                        <Button onClick={props.onClick}>OK</Button>
+                    </footer>
+                </Card>), 
+            document.getElementById('overlay-root'))}
         </React.Fragment>
     );
 };
